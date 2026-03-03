baf53a41b044903932b2b8829259bf1fc81b8d18
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-03 15:43:22 +0530

    Comparsion operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,11 @@
+// Comparsion operators
+console.log("Comparsion operators");
+let x=3 ,y='3';
+console.log("x==y: ",x==y); //loose equlity
+console.log("x===y: ",x===y); //strict equlity
+console.log("x!=y: ",x!=y); //loose inequlity
+console.log("x!==y: ",x!==y); //strict inequlity
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);

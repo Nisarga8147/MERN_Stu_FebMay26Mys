262f52e77f0edbe794f7c0ff0de77b92c59048da
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-03 15:43:50 +0530

    Logical operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,6 @@
+//Logical operator
+console.log("Logical operator");
+let a=true ,b=false;
+console.log("a&&b:",a&&b); // logical AND 
+console.log("a||b:",a||b); // logical OR
+console.log("!a:",!a); // logical NOT

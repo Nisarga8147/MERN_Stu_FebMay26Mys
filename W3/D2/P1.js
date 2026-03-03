7f969c36e4ecaae7644057e17ba682af29486dcb
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-03 15:42:39 +0530

    Arithmetic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+// Arithmetic Operators
+console.log("Arithmetic Operators");
+const a=2,b=3;
+let c=2;
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b); //modulus
+console.log("a**b: ",a**b); //exponential
+console.log("a++: ",a++); //post-increment
+console.log("++a: ",++a); //pre-increment
+console.log("a--: ",a--); //post-decrement 
+console.log("--a: ",--a); //pre-decrement

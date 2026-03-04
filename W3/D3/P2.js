ce1253cb3a9335d83bb6643df45e860bd8fffb4f
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-04 13:39:18 +0530

    while and do-while loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,15 @@
+//While loop
+console.log("while loop");
+let count=0;
+while(count<5){
+    console.log("count:",count);
+    count++;
+}
+
+//do-while loop
+console.log("d0-while loop");
+let value = 0;
+do{
+    console.log("value:",value);
+    value++;
+}while(value<3);

cfb90f74d56202145630bca6c3273b6e0ea8bf66
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-09 08:57:08 +0530

    Keyboard event:keydown

---
+++ W3/D5/P4.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("nameInput");
+const display = document.getElementById("display");
+input.addEventListener("keydown",function(event){
+    console.log("Key pressed:",event.key);
+    display.textContent = "last key pressed:"+event.key;
+});

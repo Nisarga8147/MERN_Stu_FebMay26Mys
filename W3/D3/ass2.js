aa62c065217deb24b148f8f29f4aa458faa85403
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-10 08:51:27 +0530

    Build a compact invoice using rest+default

---
+++ W3/D3/ass2.js
@@ -0,0 +1,22 @@
+function invoice(gstrate = 0.18, ...items) {
+    let subtotal = 0;
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        if (item.name === "stop") {
+            break;
+        }
+        if (typeof item.price !== "number" || typeof item.qty !== "number" ) {
+            continue;
+        }
+        subtotal += item.price * item.qty;
+    }
+    let gst = subtotal * gstrate;
+    let total = subtotal + gst;
+    return {
+        Subtotal: subtotal,
+        Gst: gst,
+        Total: total
+    };
+}
+let result = invoice(0.18,{ name: "book", price: 50, qty: 2 },);
+console.log(result);

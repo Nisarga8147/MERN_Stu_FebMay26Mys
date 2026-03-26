5741e05482c995b87cede604ee296d7b29823d49
Nisarga8147 <nisarganisu4909@gmail.com>
2026-03-26 15:57:25 +0530

    Understand the HTTP module

---
+++ W6/D4/P1.js
@@ -0,0 +1,6 @@
+//Understand the HTTP module
+
+//Importing built-in hhttp module
+const http = require("http");
+console.log("Type of http.createServer:",typeof http.createServer);
+console.log("Common HTTP methods: ",http.METHODS);

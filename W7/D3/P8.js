e59ea49cbb49ecced2212bc8d791d88bcd1f2184
Nisarga8147 <nisarganisu4909@gmail.com>
2026-04-02 12:51:24 +0530

    JWT flow with login, refresh-style logic and secure verification

--- W7/D3/P8.js
+++ W7/D3/P8.js
@@ -37,7 +37,9 @@ function authenticateAccessToken(req,res,next){
         });
     }
 }
-app.post("/login",function(req,res){
+app.post("/login",function(req,res){ 
+    //curl -X POST http://localhost:4000/login -H "Content-Type:application/json" 
+    // -d "{\"email\":\"email@email.com\",\"password\":\"pass@123\"}" 
     const {email,password} = req.body;
     if(email!=="email@email.com" || password!=="pass@123"){
         return res.status(401).json({
@@ -67,6 +69,10 @@ res.json({
 });
 
 app.post("/refresh",function(req,res){
+    //curl -X POST http://localhost:4000/refresh -H "Content-Type:application/json" -d 
+    // "{\"refreshToken\":\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwMSwiZW1haWwiOiJlbWFpbEBlbWF
+    // pbC5jb20iLCJpYXQiOjE3NzUxMTM4MDgsImV4cCI6MTc3NTk3NzgwOCwiaXNzIjoiand0LWV4YW1wbGUifQ.
+    // i6jgoXp81_nQbqciThA6QnCtZCKQ-OaTtF-afdzBVqc\"}"
     const {refreshToken} = req.body;
     if(!refreshToken || !refreshTokens.includes(refreshToken)){
         return res.status(401).json({
@@ -98,6 +104,9 @@ app.post("/refresh",function(req,res){
     }
 });
 app.get("/me",authenticateAccessToken,function(req,res){
+    //curl http://localhost:4000/me -H "Authorization:Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+    // eyJ1c2VySWQiOjEwMSwiZW1haWwiOiJlbWFpbEBlbWFpbC5jb20iLCJyb2xlIjoibWVtYmVyIiwiaWF0IjoxNzc1MTEzODU1LCJleHAiOjE3NzUxMTQ3NTUsImlzcyI6Imp3dC1leGFtcGxlIn0.
+    // R2mQMipE65brdqtkBQu90c8nlBpssBkNMDQXtpHIZRg"
     res.json({
         success:true,
         message:"Protected user route",

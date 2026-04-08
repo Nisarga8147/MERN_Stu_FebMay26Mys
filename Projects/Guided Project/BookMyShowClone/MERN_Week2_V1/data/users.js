51ea47c8325aaa65e625e3d887fcc7862860f34e
Nisarga8147 <nisarganisu4909@gmail.com>
2026-04-08 10:07:08 +0530

    User's details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,18 @@
+// User's details of the application
+const users = [
+    {
+        id:1,
+        name:"Nisarga",
+        email:"u@e.com",
+        password:"nisa123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+];
+module.exports = users;

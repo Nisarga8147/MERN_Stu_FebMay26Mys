f2942d295c5fdee83bc3b4907268539d41555948
Nisarga8147 <nisarganisu4909@gmail.com>
2026-04-23 13:02:16 +0530

    Created show schema , model and exported model

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+// Sub schema
 const seatSchema = new mongoose.Schema({
     seatNumber:{
         type:String,
-        required:true
+        required:true,
     },
     isBooked:{
         type:Boolean,
-        default:false
+        default:false,
     },
 },{_id:false});
 
@@ -15,45 +16,52 @@ const showSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"Movie",
         required:true,
-        index:true
+        index:true,
     },
     date:{
         type:Date,
         required:true,
-        index:true
+        index:true,
     },
     time:{
         type:String,
-        required:true
+        required:true,
     },
     totalSeats:{
         type:Number,
-        required:true
+        required:true,
     },
     availableSeats:{
         type:Number,
-        required:true
+        required:true,
     },
     seats:{
         type:[seatSchema],
-        required:true
+        required:true,
     },
     isActive:{
         type:Boolean,
-        default:true
+        default:true,
     },
-},{
-    timestamps:true
+},
+{
+    timestamps:true,
 });
 
-//Compound index
+//Compound index:
 showSchema.index({movieId:1,date:1});
 
 //Add Validation
-showSchema.pre("save",function(next){
-    if(this.availableSeats>this.totalSeats){
-        return next(new Error("Available seats cannot exceed total seats"));
+// showSchema.pre("save",function(next){
+//     if (this.availableSeats>this.totalSeats) {
+//         return next(new Error("Available seats cannot exceed total seats"));
+//     }
+//     next();
+// });
+// New Add Validation code
+showSchema.pre("save", async function () {
+    if (this.availableSeats > this.totalSeats) {
+        throw new Error("Available seats cannot exceed total seats");
     }
-    next();
 });
 module.exports = mongoose.model("Show",showSchema);

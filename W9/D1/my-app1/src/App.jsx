9ee59a5179f14e87ef939ace4aff6002d6055765
Nisarga8147 <nisarganisu4909@gmail.com>
2026-04-28 09:53:13 +0530

    expressions within JSX

--- W9/D1/my-app1/src/App.jsx
+++ W9/D1/my-app1/src/App.jsx
@@ -118,12 +118,24 @@ import './App.css'
 //     </>
 //   )
 // }
+// function App(){
+//             return(
+//                 <div>
+//                     <h1>React Introduction</h1>
+//                     <p> React simplifies UI creation through reusable components.</p>
+//                 </div>
+//             );
+//         }
 function App(){
-            return(
-                <div>
-                    <h1>React Introduction</h1>
-                    <p> React simplifies UI creation through reusable components.</p>
-                </div>
-            );
-        }
+    const user = "Nisarga";
+    const marks = 70;
+    return(
+        // expressions within JSX
+        <div>
+        {/* {curly brackets}: in JSX means: switch from JSX/HTML-like syntax into JavaScript */}
+        <h1>Hello,{user}</h1>
+        <p>Updated marks : {marks+10}</p>
+        </div>
+    ); 
+}
 export default App

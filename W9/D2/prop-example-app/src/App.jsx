1b279cb70ced7b9cc9a25b0e5acab5183cf6f9b3
Nisarga8147 <nisarganisu4909@gmail.com>
2026-04-29 15:13:45 +0530

    Higher order components(HOC)

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -10,6 +10,7 @@ import { PropsImmutability } from './components/P8'
 import { PropsState } from './components/P9'
 import { FunctionProps } from './components/P10'
 import { PropTypesDemo } from './components/P11'
+import { HOCExample } from './components/P12'
 import './App.css'
 
 function App(){
@@ -25,7 +26,8 @@ function App(){
     {/* <PropsImmutability /> */}
     {/* <PropsState /> */}
     {/* <FunctionProps /> */}
-    <PropTypesDemo />
+    {/* <PropTypesDemo /> */}
+    <HOCExample />
     </>
   )
 }

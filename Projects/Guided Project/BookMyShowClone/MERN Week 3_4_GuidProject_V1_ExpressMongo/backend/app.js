5a011a16ca807c20e6df76e9e4ab8cde72c86988
Nisarga8147 <nisarganisu4909@gmail.com>
2026-05-04 09:39:23 +0530

    Added shows routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,15 +1,19 @@
 
 const express = require("express");
+const morgan = require("morgan");
 const authRoutes = require("./src/routes/auth.routes");
 const movieRoutes = require("./src/routes/movie.routes");
 const bookingRoutes = require("./src/routes/booking.routes");
 const showRoutes = require("./src/routes/show.routes");
+const errorMiddleware = require("./src/middleware/error.middleware");
 const app = express();
 app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/movies",movieRoutes);
 app.use("/api/bookings",bookingRoutes);
 app.use("/api/shows",showRoutes);
+app.use(morgan("dev"));
+app.use(errorMiddleware);
 //Base URL
 app.get("/",(req,res)=>{
     res.status(200).json({

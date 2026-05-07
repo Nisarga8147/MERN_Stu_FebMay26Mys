f7621ce9be72a5fe18ccc1e6833248d5110a043f
Nisarga8147 <nisarganisu4909@gmail.com>
2026-05-07 21:35:15 +0530

    changes done in portfolio

--- W4/D5/portfolio-project/js/features/theme-toggle.js
+++ W4/D5/portfolio-project/js/features/theme-toggle.js
@@ -1,18 +1,7 @@
-function initThemeToggle(){
-    const  toggleBtn = document.getElementById("theme-toggle");
-    const body = document.body;
-    const savedTheme = localStorage.getItem("portfolio-theme");
-    if(savedTheme === "dark"){
-        body.classList.add("dark-mode");
-    }
-    toggleBtn.addEventListener("click",function(){
-        body.classList.toggle("dark-mode");
-        if(body.classList.contains("dark-mode")){
-            localStorage.setItem("portfolio-theme","dark");
-            console.log("Dark mode enabled");
-        }else{
-            localStorage.setItem("portfolio-theme","light");
-            console.log("Light mode enabled");
-        }
-    });
+function setupThemeToggle() {
+  const btn = document.getElementById("theme-btn");
+
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
 }
